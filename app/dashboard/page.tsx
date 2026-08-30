990dcef3fc9c5ae8520a2783a87a4d73ad656095
Alexandr <alexpryp@gmail.com>
2026-08-31 00:17:30 +0300

    ADD | Recent Revenue chart on Home page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-  return <p>Dashboard Page</p>
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue } from '@/app/lib/data';
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols2 lg:grid-cols-4">
+        <div>Card</div>
+        <div>Card</div>
+        <div>Card</div>
+        <div>Card</div>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+      </div>
+    </main>
+  );
 }
